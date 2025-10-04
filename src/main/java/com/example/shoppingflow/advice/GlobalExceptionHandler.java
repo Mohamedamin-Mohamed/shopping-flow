18381572f0db26dee45b02ad75bdef5a06287f32
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2025-10-03 19:19:28 -0500

    feat: controller advice class to intercept various exceptions

--- src/main/java/com/example/shoppingflow/advice/GlobalExceptionHandler.java
+++ src/main/java/com/example/shoppingflow/advice/GlobalExceptionHandler.java
@@ -1,4 +1,24 @@
 package com.example.shoppingflow.advice;
 
+import com.example.shoppingflow.DTO.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
+        ErrorResponse errorResponse = buildErrorResponse(e);
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+
+    private ErrorResponse buildErrorResponse(Exception e) {
+        return new ErrorResponse(e.getMessage(), LocalDateTime.now().toString(), 400, "Bad request");
+    }
 }
