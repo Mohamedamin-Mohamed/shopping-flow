d3c0135ad38457d53ff94cf1b94a7dceb58a885b
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2025-10-03 19:17:15 -0500

    feat: add getter for totalQuantity field

--- src/main/java/com/example/shoppingflow/DTO/InventoryInfo.java
+++ src/main/java/com/example/shoppingflow/DTO/InventoryInfo.java
@@ -1,8 +1,41 @@
 package com.example.shoppingflow.DTO;
 
-public class Inventory {
-    private long storeId;
+public class InventoryInfo {
+    private String storeId;
     private String tsin;
     private long totalQuantity;
     private long reservedQuantity;
+
+    public void setStoreId(String storeId) {
+        this.storeId = storeId;
+    }
+
+    public void setTsin(String tsin) {
+        this.tsin = tsin;
+    }
+
+    public void setTotalQuantity(long totalQuantity) {
+        this.totalQuantity = totalQuantity;
+    }
+
+    public void setReservedQuantity(long reservedQuantity) {
+        this.reservedQuantity = reservedQuantity;
+    }
+
+    public long getStoreId() {
+        return storeId;
+    }
+
+    public String getTsin() {
+        return tsin;
+    }
+
+    public long getTotalQuantity() {
+        return totalQuantity;
+    }
+
+    public long getReservedQuantity() {
+        return reservedQuantity;
+    }
+
 }
