5df7a882076725ab9b0b899610f0be7ed05bf93e
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2025-10-03 19:21:43 -0500

    feat: add new method to enhance partial product update

--- src/main/java/com/example/shoppingflow/repository/CatalogRepository.java
+++ src/main/java/com/example/shoppingflow/repository/CatalogRepository.java
@@ -1,10 +1,11 @@
 package com.example.shoppingflow.repository;
 
 import com.example.shoppingflow.DTO.Product;
+import com.example.shoppingflow.DTO.ProductPatchRequest;
 import com.example.shoppingflow.config.UtilConfig;
-import com.example.shoppingflow.interfaces.DynamoQueryable;
 import com.example.shoppingflow.model.Catalog;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Repository;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbAsyncTable;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedAsyncClient;
@@ -16,11 +17,13 @@ import software.amazon.awssdk.enhanced.dynamodb.model.QueryEnhancedRequest;
 
 import java.util.concurrent.CompletableFuture;
 
+import static com.example.shoppingflow.utils.Util.convertToProduct;
+
 @Repository
 public class CatalogRepository implements DynamoQueryable {
     private DynamoDbEnhancedAsyncClient dynamoClient;
     private UtilConfig utilConfig;
-
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     public CatalogRepository(DynamoDbEnhancedAsyncClient dynamoDbEnhancedClient, UtilConfig utilConfig) {
         this.dynamoClient = dynamoDbEnhancedClient;
@@ -38,6 +41,23 @@ public class CatalogRepository implements DynamoQueryable {
     @Override
     public CompletableFuture<Catalog> updateProduct(Product product) throws JsonProcessingException {
         return dynamoDbAsyncTable.updateItem(convertToCatalog(product));
+    }
+
+    @Override
+    public CompletableFuture<Catalog> updateProductPartial(String tsin, ProductPatchRequest productPatchRequest) {
+        /*here first get the old catalog, convert it to product, then update that product with productPatchRequest */
+        return getProduct(tsin).thenCompose(catalog -> {
+            try {
+                Product product = convertToProduct(catalog);
+                //overlay productPatchRequest on top of the existing product meaning it takes only non-null fields
+                product = objectMapper.updateValue(product, productPatchRequest);
+                Catalog newCatalog = convertToCatalog(product);
+
+                return dynamoDbAsyncTable.updateItem(newCatalog);
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 
     @Override
@@ -59,7 +79,7 @@ public class CatalogRepository implements DynamoQueryable {
         return Key.builder().partitionValue(keyName).build();
     }
 
-    private Key partitionSortKeyBuilder(String partitionKey, String sortKey){
+    private Key partitionSortKeyBuilder(String partitionKey, String sortKey) {
         return Key.builder().partitionValue(partitionKey).sortValue(sortKey).build();
     }
 
