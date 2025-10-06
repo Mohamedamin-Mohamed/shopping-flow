e42bb9e4fcf0722510be2b7e9cc2976c296bc5d0
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2025-10-05 21:31:43 -0500

    feat: add new exception handler class

--- src/main/java/com/example/shoppingflow/advice/GlobalExceptionHandler.java
+++ src/main/java/com/example/shoppingflow/advice/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.example.shoppingflow.advice;
 
 import com.example.shoppingflow.DTO.ErrorResponse;
+import com.example.shoppingflow.Exception.ClientException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -13,12 +14,22 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
-        ErrorResponse errorResponse = buildErrorResponse(e);
+        return createResponse(e);
+    }
 
+    @ExceptionHandler(ClientException.class)
+    public ResponseEntity<ErrorResponse> handleClientSideException(ClientException clientException) {
+        return createResponse(clientException);
+    }
+
+    private ResponseEntity<ErrorResponse> createResponse(Exception e) {
+        ErrorResponse errorResponse = buildErrorResponse(e);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 
     private ErrorResponse buildErrorResponse(Exception e) {
         return new ErrorResponse(e.getMessage(), LocalDateTime.now().toString(), 400, "Bad request");
     }
+
+
 }
