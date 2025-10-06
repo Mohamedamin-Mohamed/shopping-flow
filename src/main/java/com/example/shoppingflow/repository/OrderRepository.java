d244836a7c88163275edd05de4d367ba096fdd56
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2025-10-05 21:35:09 -0500

    feat: add new method find order by id

--- src/main/java/com/example/shoppingflow/repository/OrderRepository.java
+++ src/main/java/com/example/shoppingflow/repository/OrderRepository.java
@@ -1,7 +1,14 @@
 package com.example.shoppingflow.repository;
 
+import com.example.shoppingflow.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+
 @Repository
-public class OrderRepository {
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    CompletableFuture<Order> saveOrder(Order order);
+    CompletableFuture<Order> findByOrderId(UUID orderId);
 }
